ca6ce485f5017bbbbb249642b0bea7d3a05142f8
Ahmad Fadhliansyah <91241480+fadhliansyah@users.noreply.github.com>
2022-03-22 14:40:35 +0700

    Add files via upload

---
+++ atas.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <title>SAS-STTNF</title>
+</head>
+<body>
+<header class="text-center"role="banner">
+    <h2>Student Activity Score - STTNF</h2>
+    <hr>
+</header>
